27f1465606cd76a8c72916e2da2c9972bf8ffaf0
data-live-ops <imam.fachrudin@colearn.id>
2025-10-30 15:11:16 +0700

    fix: correct admin password variable in Odoo entrypoint script

--- docker-entrypoint-railway.sh
+++ docker-entrypoint-railway.sh
@@ -55,6 +55,6 @@ exec odoo \
     --limit-time-real="${LIMIT_TIME_REAL}" \
     --without-demo=all \
     --log-level=info \
-    --admin-password="${ADMIN_PASSWORD}" \
+    --admin-password="${ADMIN_PASSWD}" \
     --server-wide-modules=base,web,queue_job \
     "$@"
